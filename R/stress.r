705cdda21417c470b6cab5283263c2ad497d9fed
Drew Schmidt <schmidt@math.utk.edu>
2015-03-02 20:47:19 -0500

    moved to roxygen2

--- R/stress.r
+++ R/stress.r
@@ -1,3 +1,38 @@
+#' @name options
+#' @rdname options
+#' 
+#' @title Stress Tester Options
+#' 
+#' @description
+#' TODO.
+#' 
+#' @details
+#' TODO.
+#' 
+#' @aliases stress.opts vm.opts
+#' @param backoff
+#' Wait time in microseconds before starting.
+#' @param verbose 
+#' .
+#' @param dryrun 
+#' .
+#' @param debug 
+#' Show what would have been done without actually doing it.
+#' @param bytes 
+#' The number of bytes allocated by each vm worker.
+#' @param stride 
+#' Touch a byte every \code{stride} bytes.
+#' @param hang 
+#' Time to sleep before calling free.
+#' @param keep 
+#' Option to re-use memory rather than freeing and reallocating.
+#' 
+#' @return
+#' TODO.
+#' 
+#' @seealso \code{\link{stress}}
+#' 
+#' @export
 stress.opts <- function(backoff=3600, verbose=TRUE, dryrun=FALSE, debug=FALSE)
 {
   assert.wholenum(backoff)
@@ -22,7 +57,9 @@ stress.opts <- function(backoff=3600, verbose=TRUE, dryrun=FALSE, debug=FALSE)
 }
 
 
-
+#' @rdname options
+#' 
+#' @export
 vm.opts <- function(bytes=256*1024*1024, stride=4096, hang=0, keep=FALSE)
 {
   assert.wholenum(bytes)
@@ -50,6 +87,55 @@ vm.opts <- function(bytes=256*1024*1024, stride=4096, hang=0, keep=FALSE)
 
 
 
+
+#' @name stress
+#' @rdname stress
+#' 
+#' @title Stress Tester
+#' 
+#' @description 
+#' TODO.
+#' 
+#' @details
+#' TODO
+#' 
+#' \tabular{ll}{ cpu \tab spins on \code{sqrt} \cr io \tab spins on \code{sync}
+#' \cr vm \tab spins on \code{malloc/free} \cr hdd \tab spins on
+#' \code{write/unlink} \cr }
+#' 
+#' @aliases stress stress.cpu stress.io stress.vm stress.hdd
+#' 
+#' @param timeout 
+#' Run time for the test in seconds.  If 0, the test will run
+#' forever (and may be impossible to kill from the R console!).
+#' @param cpu 
+#' The number of CPU workers to spawn.
+#' @param io 
+#' The number of IO workers to spawn.
+#' @param vm 
+#' The number of memory workers to spawn.
+#' @param hdd 
+#' The number of writers to spawn.
+#' @param hdd_bytes 
+#' The number of bytes written per hdd worker.
+#' @param vmopts 
+#' .
+#' @param opts 
+#' .
+#' 
+#' @return Each function returns \code{NULL}.
+#' 
+#' @seealso \code{\link{options}}
+#' 
+#' @examples
+#' 
+#' \dontrun{
+#' library(stressR)
+#' 
+#' stress.cpu(cpu=1)
+#' }
+#' 
+#' @export stress
 stress <- function(timeout=1, cpu=0, io=0, vm=0, hdd=0, hdd_bytes=1024*1024*1024, vmopts=vm.opts(), opts=stress.opts())
 {
   timeout <- as.integer(timeout)
@@ -74,28 +160,36 @@ stress <- function(timeout=1, cpu=0, io=0, vm=0, hdd=0, hdd_bytes=1024*1024*1024
 }
 
 
-
+#' @rdname stress
+#' 
+#' @export
 stress.cpu <- function(timeout=1, cpu=1, opts=stress.opts())
 {
   stress(timeout=timeout, cpu=cpu, io=0, vm=0, hdd=0, opts=opts)
 }
 
 
-
+#' @rdname stress
+#' 
+#' @export
 stress.io <- function(timeout=1, io=1, opts=stress.opts())
 {
   stress(timeout=timeout, cpu=0, io=io, vm=0, hdd=0, opts=opts)
 }
 
 
-
+#' @rdname stress
+#' 
+#' @export
 stress.vm <- function(timeout=1, vm=1, vmopts=vm.opts(), opts=stress.opts())
 {
   stress(timeout=timeout, cpu=0, io=0, vm=vm, hdd=0, vmopts=vmopts, opts=opts)
 }
 
 
-
+#' @rdname stress
+#' 
+#' @export
 stress.hdd <- function(timeout=1, hdd=1, hdd_bytes=1024*1024*1024, opts=stress.opts())
 {
   stress(timeout=timeout, cpu=0, io=0, vm=0, hdd=hdd, hdd_bytes=hdd_bytes, opts=opts)
---
+++ R/stressR-package.R
@@ -0,0 +1,18 @@
+#' @name stressR
+#' @title stressR-package
+#' 
+#' @description
+#' TODO.
+#' 
+#' @details
+#' TODO.
+#' 
+#' \tabular{ll}{ Package: \tab stressR\cr Type: \tab Package\cr License: \tab
+#' GPL (>= 2)\cr }
+#' 
+#' @docType package
+#' @author Drew Schmidt \email{wrathematics AT gmail.com}
+#' @references \url{https://github.com/wrathematics/stressR}
+#' @keywords Package
+NULL
+
